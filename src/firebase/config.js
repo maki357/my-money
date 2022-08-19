5eb7388e49d5638d8528955ea8564de044d35bd5
Mario Stosic <65606596+maki357@users.noreply.github.com>
2022-08-19 19:10:44 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCnYOFDxiwd-GH0zYwMatJZqfFVMbVDYKM",
+  apiKey: API_KEY,
   authDomain: "mymoney-wallet-b9103.firebaseapp.com",
   projectId: "mymoney-wallet-b9103",
   storageBucket: "mymoney-wallet-b9103.appspot.com",
   messagingSenderId: "751694315632",
-  appId: "1:751694315632:web:e261a39468af28ceef74b5",
+  appId: API_ID,
 };
 
 // init firebase
